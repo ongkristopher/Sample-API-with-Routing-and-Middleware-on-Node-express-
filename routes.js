789ac43c7677054f4d62ac25bf0695dba4542f05
Kristopher Ong <ongkristopher@gmail.com>
2020-01-12 16:46:39 +0800

    udpdated files

--- routes.js
+++ routes.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const error = require('./middleware/error');
 const articles = require('./api/articles');
 
 module.exports = function (app) {
   app.use(express.json());
   app.use('/articles', articles);
-  app.use(error);
 }
